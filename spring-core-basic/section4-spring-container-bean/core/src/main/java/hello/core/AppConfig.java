ecee2b00293f53205da41092cce5147a78fb6f7c
hyeWon <35442383+hyeWon0324@users.noreply.github.com>
2022-04-25 00:53:06 +0900

    [section4] 빈 조회 (find Bean)

--- spring-core-basic/section4-spring-container-bean/core/src/main/java/hello/core/AppConfig.java
+++ spring-core-basic/section4-spring-container-bean/core/src/main/java/hello/core/AppConfig.java
@@ -9,19 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //설정 정보를 말한다.
 public class AppConfig {
+    @Bean   //각 함수를 빈으로 등록하면 스프링 컨테이너에 들어간다
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixedDiscountPolicy();
         return new RateDiscountPolicy();
--- spring-core-basic/section4-spring-container-bean/core/src/main/java/hello/core/MemberApp.java
+++ spring-core-basic/section4-spring-container-bean/core/src/main/java/hello/core/MemberApp.java
@@ -4,24 +4,37 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); //appConfig 에서 결정하도록 한다
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); //appConfig 에서 결정하도록 한다
         //순수한 자바 코드
 //        MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "memberA", Grade.VIP); //ctrl+alt+v
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP); //ctrl+alt+v
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new memeber = " + member.getName());
         System.out.println("find memeber = " + findMember.getName());
-
         //출력
 //        new member = memberA
 //        find Member = memberA
+
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
+        System.out.println("order = " + order);
+
 
     }
 }
