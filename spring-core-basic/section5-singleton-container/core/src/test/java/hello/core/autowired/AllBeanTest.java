78cbbb3a9acd27ae1fb9a27b314e6bdc0c3d0e09
hyeWon0324 <parkhw0324@gmail.com>
2022-05-29 20:32:50 +0900

    [section7] 조회한 빈이 모두 필요할 때, List,Map

---
+++ spring-core-basic/section5-singleton-container/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,47 @@
+package hello.core.autowired;
+
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class)
+
+        DiscountService discountService = ac.getBean(DiscountService.calss) ;
+        Memeber member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000,"fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 2000,"rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+
+        }
+    }
+
+    public int discount(Member member, int price, String discountCode) {
+        DiscountPolicy discountPolicy = policyMap.get(discountCode);
+        return discountPolicy.discount(member, price);
+    }
+}
