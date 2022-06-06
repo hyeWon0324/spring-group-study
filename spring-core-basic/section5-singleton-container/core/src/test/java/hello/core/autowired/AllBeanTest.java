61e70ddcda771ffb5185283f2490a99273f3e1d1
hyeWon <35442383+hyeWon0324@users.noreply.github.com>
2022-06-06 22:05:42 +0900

    [section8] interface InitializingBean, DisposableBean

--- spring-core-basic/section5-singleton-container/core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ spring-core-basic/section5-singleton-container/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,21 +1,28 @@
 package hello.core.autowired;
 
 
+import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AllBeanTest {
 
     @Test
     void findAllBean() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class)
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
 
-        DiscountService discountService = ac.getBean(DiscountService.calss) ;
-        Memeber member = new Member(1L, "userA", Grade.VIP);
+        DiscountService discountService = ac.getBean(DiscountService.class) ;
+        Member member = new Member(1L, "userA", Grade.VIP);
         int discountPrice = discountService.discount(member, 10000,"fixDiscountPolicy");
 
         assertThat(discountService).isInstanceOf(DiscountService.class);
@@ -38,10 +45,11 @@ public class AllBeanTest {
             System.out.println("policies = " + policies);
 
         }
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
     }
 
-    public int discount(Member member, int price, String discountCode) {
-        DiscountPolicy discountPolicy = policyMap.get(discountCode);
-        return discountPolicy.discount(member, price);
-    }
+
 }
--- spring-core-basic/section5-singleton-container/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-core-basic/section5-singleton-container/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,15 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean {
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자를 호출, url= " + url);
-        connect();
-        call("Initialized Connect Message");
+//        connect();
+//        call("Initialized Connect Message");
     }
 
     public void setUrl(String url) {
@@ -29,8 +30,16 @@ public class NetworkClient implements InitializingBean {
         System.out.println("close: " + url);
     }
 
-    @Override
+    @Override //InitializingBean
     public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("Initialized Connect Message");
+    }
 
+    @Override //DisposableBean
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
